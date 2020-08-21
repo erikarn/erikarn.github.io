9ea3a40d10fec31cf0f59fccc0d7c74c9bb9d85a
Adrian Chadd <adrian@freebsd.org>
2020-08-21 10:42:21 -0700

    Futzing

--- kenwood/SB/fetch.sh
+++ kenwood/SB/fetch.sh
@@ -5,3 +5,5 @@ for i in `seq -f '%04g' 1 2000`; do
 	curl -o ASB${i}.pdf https://www.kenwood.com/usa/Support/pdf/ASB${i}.pdf
 
 done
+
+# ls *pdf | perl -ne 'while (<>) { chomp; printf " * [%s](%s)\n", $_, $_; }'
